84f788de0d5266b79f4e37f7eed4b62d1559d1c7
shivani-pansala <143687310+shivani-pansala@users.noreply.github.com>
2024-09-06 08:44:37 +0530

    Add files via upload

---
+++ Resume/Resume.js
@@ -0,0 +1,115 @@
+import React from 'react'
+
+ function Resume() {
+  return (
+    <div>
+      <div className='container'>
+          <div style={{'padding':'30px','backgroundColor':'#8ddaf0',"borderBottom":"3px solid #21a2bf"}}>
+               <div style={{'display':'flex',"marginLeft":"30%"}}>
+               <h6 style={{"borderRight":"3px solid #21a2bf","padding":"0px 30px"}}>New Delhi,India 110034</h6>
+               <h6 style={{"borderRight":"3px solid #21a2bf" ,"padding":"0px 30px"}}>+91 22 5555 3345</h6>
+               <h6 style={{"padding":"0px 30px"}}>i.agarwal@sample.in</h6>
+          </div>
+          </div>
+          
+          {/* <hr style={{'color':'blue'}}></hr> */}
+          <div style={{"borderBottom":"3px solid #21a2bf"}}>
+              <h4 style={{'textAlign':'center','margin':'40px',"fontSize":"50PX"}}>ISHAAN AGARWAL</h4>
+              <div style={{"backgroundColor":"#a8e3f0","width":"200px","height":"10px"}}></div>
+         </div>
+          <div className='row' style={{"borderBottom":"3px solid #21a2bf","margin":"10px 0px"}}>
+               <div className='col-2'>
+                    <h5>PROFESSIONAL SUMMARY</h5>
+               </div>
+               <div className='col-9'>
+                   <p style={{'fontSize':''}}>
+                    Customer:focused Reatail & Sales professional with in -depth undersanding of Reatail dynamics,marketing and customer service.
+                    hfuiwhfjnvrvnj verhhjdnvjkxnviurenvjkdnxvj kndrovnr jurhvioerhvjkadnfvjkd fghbvuisehbiosrndfklb fd fbiutshjbrsnbke dfjbvuiershgiaerv 
+                    vhrgheiurjnvbjes rgvherihe brvuigerhbvaew evuirehbveor fbvaeuirhbv4e dbviuearbvier bvuieiar rbgvarevne vbieurvhboiqe4 ebcfiuwehfo3 ebcigwefiwe
+                    jfhuierhviaoer nsbfreiuqvh vbkewhvoaiew cbuaeivhb vbaieuvhew dcbiaeuvh bcWV 
+                   </p>
+              </div>
+              <div style={{"backgroundColor":"#a8e3f0","width":"200px","height":"10px"}}></div>
+          </div>
+          <div className='row'  style={{"borderBottom":"3px solid #21a2bf"}}>
+               <div className='col-2'>
+                  <h5>CORE QUALIFICATION</h5>
+               </div>
+            <div className='col-9'>
+              <div className='row'>
+                 <div className='col-4'>
+                       <ul>
+                         <li>Cash register operation</li>
+                         <li>Pos system operation</li>
+                         <li>Sales expertise</li>
+                       </ul>
+                 </div>
+                 <div className='col-4'>
+                 <ul>
+                       <li>Teamwork</li>
+                       <li>Inventory management</li>
+                       <li>Accurate money handling</li>
+                  </ul>
+                </div>
+                <div className='col-4'>
+                <ul>
+                   <li></li>Documentation & record-keeping
+                   <li>Reatail merchandising expertise</li>
+                </ul>
+                </div>
+                </div>
+            </div>
+            <div style={{"backgroundColor":"#a8e3f0","width":"200px","height":"10px"}}></div>
+          </div>
+          <div className='row' style={{"borderBottom":"3px solid #21a2bf"}}>
+               <div className='col-2'>
+                    <h5>EXPERIENCE</h5>
+               </div>
+               <div className='col-9'>
+                      <b>RETAIL SALES ASSOCIATE</b> 02/2021 to Current<br/>
+                      <b>  ZARA  </b>New Delhi,India
+                  <ul>
+                     <li>Incresed monthly sales 10% by effectively upselling and cross-selling Products to <br/>maximise Profitability </li>
+                     <li>Incresed monthly sales 10% by effectively upselling and cross-selling Products to <br/>maximise Profitability </li>
+                     <li>Incresed monthly sales 10% by effectively upselling and cross-selling Products to maximise Profitability </li>
+                  </ul>
+                  
+                  <b>RETAIL SALES ASSOCIATE</b> 02/2021 to Current<br/>
+                      <b>  ZARA  </b>New Delhi,India
+                    <ul>
+                     <li>Incresed monthly sales 10% by effectively upselling and cross-selling Products to maximise Profitability </li>
+                     <li>Incresed monthly sales 10% by effectively upselling and cross-selling Products to <br/>maximise Profitability </li>
+                     <li>Incresed monthly sales 10% by effectively upselling and cross-selling Products to maximise Profitability </li>
+                     <li>Incresed monthly sales 10% by effectively upselling and cross-selling Products to <br/>maximise Profitability </li>
+                   </ul>
+
+                   <b>RETAIL SALES ASSOCIATE</b> 02/2021 to Current<br/>
+                   <b>  ZARA  </b>New Delhi,India
+                 <ul>
+                  <li>Incresed monthly sales 10% by effectively upselling and cross-selling Products to <br/>maximise Profitability </li>
+                  <li>Incresed monthly sales 10% by effectively upselling and cross-selling Products to <br/>maximise Profitability </li>
+                  <li>Incresed monthly sales 10% by effectively upselling and cross-selling Products to maximise Profitability </li>
+                  <li>Incresed monthly sales 10% by effectively upselling and cross-selling Products to maximise Profitability </li>
+                  <li>Incresed monthly sales 10% by effectively upselling and cross-selling Products to maximise Profitability </li>
+                  <li>Incresed monthly sales 10% by effectively upselling and cross-selling Products to <br/>maximise Profitability </li>
+                </ul>
+              </div>
+              <div style={{"backgroundColor":"#a8e3f0","width":"200px","height":"10px"}}></div>
+          </div>
+          
+          <div className='row'>
+             <div className='col-2'>
+          <h5>EDUCATION</h5>
+        </div>
+        <div className='col-9'>
+         <p style={{"marginBottom":"40PX"}}>
+          <b>Customer:focused Reatail & Sales</b>professional with in -depth undersanding <br/>of Reatail dynamics,marketing and customer service.
+         </p>
+    </div>
+</div>
+</div>
+          
+      </div>
+  )
+}
+export default Resume;
